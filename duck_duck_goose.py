07ea8f2b5659473f52f4e1b99899e0eb4c683c9c
Carla <c.deleon.2018@alumnos.urjc.es>
2019-01-21 16:24:44 +0100

    he cambia un print

--- duck_duck_goose.py
+++ duck_duck_goose.py
@@ -1,5 +1,6 @@
 def duck_duck_goose(nombres, numero):
     print("La oca es:", nombres[numero])
+    print("hola")
     return
 nombres = ["Ruben", "Laura", "Paula", "Juan", "Carla", "Pablo", "Lorena"]
 duck_duck_goose(nombres, numero = int(input("Introduzca un número:")))
