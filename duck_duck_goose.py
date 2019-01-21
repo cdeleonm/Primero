d62b3c7ab2fe339a23c5dfff1c8b7aa47b09fd22
Carla <c.deleon.2018@alumnos.urjc.es>
2019-01-21 16:18:06 +0100

    mi primer commit

---
+++ duck_duck_goose.py
@@ -0,0 +1,5 @@
+def duck_duck_goose(nombres, numero):
+    print("La oca es:", nombres[numero])
+    return
+nombres = ["Ruben", "Laura", "Paula", "Juan", "Carla", "Pablo", "Lorena"]
+duck_duck_goose(nombres, numero = int(input("Introduzca un número:")))
